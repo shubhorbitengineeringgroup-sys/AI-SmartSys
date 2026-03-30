47450f427ee5923b5dbfd40f502d1410771fd53d
shubhorbitengineeringgroup-sys <shubh.orbitengineering.group@gmail.com>
2026-03-31 02:56:46 +0530

    Initial commit with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_ACTIONS ? "/aismartsys/" : "/",
+  base: process.env.GITHUB_ACTIONS ? "/AI-SmartSys/" : "/",
   server: {
     host: "::",
     port: 8080,
